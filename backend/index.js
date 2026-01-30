69813b44e7cd5239e9ed8a40fdc8fd082bbab806
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-01-30 11:30:27 +0100

    Added: user schema and import schemas in backend
    Created an empty users schema file and updated backend/index.js to import users, answers, and questions schemas. This sets up the initial structure for schema management in the backend.

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+import schemaAnswers from './schemas/answers.js';
+import schemaQuestions from './schemas/questions.js';
+import schemaUsers from './schemas/users.js';
+
