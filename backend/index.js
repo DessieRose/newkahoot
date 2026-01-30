5f07790bb3705b31fe4dfac53119584ecde19cba
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-01-30 11:38:32 +0100

    Added: MongoDB connection and .env example
    Introduces MongoDB connection logic to index.js using the DB_CONNECTION environment variable. Adds a .env.example file to guide developers in setting up the required database connection string.

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,18 @@ import schemaAnswers from './schemas/answers.js';
 import schemaQuestions from './schemas/questions.js';
 import schemaUsers from './schemas/users.js';
 
+
+mongoose
+    .connect(
+        process.env.DB_CONNECTION,
+    )
+    .then(
+        app.listen(port, () => {
+            console.log(`NewKahoot running on port ${port}`);
+        })
+    )
+    .catch(
+        (err) => {
+            console.error('Failed to connect to MongoDB', err);
+        }
+    );
