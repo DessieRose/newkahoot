d57a933581ba37e3e64151cd390d321af995f42f
Amin El-Rifai <amin@users.noreply.github.com>
2026-02-04 13:48:50 +0100

    Added: Updated socket logic to utilize Game and Player models

--- backend/websockets/index.js
+++ backend/websockets/index.js
@@ -1,6 +1,11 @@
 import { Server } from "socket.io";
 import eiows from "eiows";
 
+import Game from "./models/Game.js";
+import Player from "./models/Player.js";
+
+const game = new Game();
+
 export function startGameserver() {
   const io = new Server(3000, {
     wsEngine: eiows.Server,
@@ -9,17 +14,21 @@ export function startGameserver() {
     },
   });
 
-  let users = [];
-
   io.on("connection", (socket) => {
     socket.on("joinGame", (playerName) => {
-      users.push({ name: playerName, id: socket.id });
-      io.emit("updateUsers", users.map((u) => u.name).sort());
+      game.addPlayer(new Player(socket.id, playerName));
+      io.emit(
+        "updateUsers",
+        game.playerEntries.map(([id, p]) => p.name).sort(),
+      );
     });
 
     socket.on("disconnect", () => {
-      users = users.filter((u) => u.id !== socket.id);
-      io.emit("updateUsers", users.map((u) => u.name).sort());
+      game.removePlayer(socket.id);
+      io.emit(
+        "updateUsers",
+        game.playerEntries.map(([id, p]) => p.name).sort(),
+      );
     });
   });
 
