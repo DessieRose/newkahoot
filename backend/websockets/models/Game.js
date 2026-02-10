ec76d42ee55fcc78bd89891ddbcea6e9b18ce297
Amin El-Rifai <amin@users.noreply.github.com>
2026-02-10 21:06:56 +0100

    Game class cleanup

--- backend/websockets/models/Game.js
+++ backend/websockets/models/Game.js
@@ -1,48 +1,16 @@
 export default class Game {
+  static STATUSES = ["idle", "active"];
+
   #players = new Map();
   #questions;
   #status = "idle";
   #currentQuestionIndex = 0;
-  #gameOver = false;
-
-  static STATUSES = ["idle", "active"];
 
   constructor(questions) {
     this.#questions = questions;
   }
 
-  getQuestion() {
-    if (this.#currentQuestionIndex >= this.#questions.length) {
-      this.#gameOver = true;
-      return null;
-    }
-    return this.#questions[this.#currentQuestionIndex++];
-  }
-
-  checkNameAvailability(name) {
-    for (const [, player] of this.#players) {
-      if (player.name.toLowerCase() === name.toLowerCase()) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  addPlayer(player) {
-    this.#players.set(player.id, player);
-  }
-
-  getPlayer(id) {
-    return this.#players.get(id);
-  }
-
-  reset() {
-    this.#players.clear();
-    this.#currentQuestionIndex = 0;
-    this.#gameOver = false;
-    this.#status = "idle";
-  }
-
+  // Status
   get status() {
     return this.#status;
   }
@@ -57,6 +25,11 @@ export default class Game {
     return this.#status;
   }
 
+  // Players
+  get playerCount() {
+    return this.#players.size;
+  }
+
   get playerEntries() {
     return [...this.#players.entries()];
   }
@@ -65,15 +38,39 @@ export default class Game {
     return [...this.#players.entries()].sort((a, b) => b[1].score - a[1].score);
   }
 
-  get gameOver() {
-    return this.#gameOver;
+  addPlayer(player) {
+    this.#players.set(player.id, player);
+  }
+
+  getPlayer(id) {
+    return this.#players.get(id);
   }
 
   removePlayer(id) {
     return this.#players.delete(id);
   }
 
-  get playerCount() {
-    return this.#players.size;
+  checkNameAvailability(name) {
+    for (const [, player] of this.#players) {
+      if (player.name.toLowerCase() === name.toLowerCase()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Questions
+  getQuestion() {
+    if (this.#currentQuestionIndex >= this.#questions.length) {
+      return null;
+    }
+    return this.#questions[this.#currentQuestionIndex++];
+  }
+
+  // Game control
+  reset() {
+    this.#players.clear();
+    this.#currentQuestionIndex = 0;
+    this.#status = "idle";
   }
 }
